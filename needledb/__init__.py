0565dfbd8dbc89faa9f0e0e97e757be5e09a1347
Mohammed Inayathulla <159543894+MohammadInayathulla@users.noreply.github.com>
2026-05-30 17:50:31 +0530

    Update __init__.py

--- needledb/__init__.py
+++ needledb/__init__.py
@@ -1,4 +1,5 @@
 from .brute_force import BruteForce
 from .kdtree import KDTree
 from .hnsw import HNSW
+from .vectordb import VectorDB, DEMO_VECTORS
 from .metrics import get_metric, cosine_distance, euclidean_distance, manhattan_distance
